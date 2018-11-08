a56ca170738d4f3c48c91bd09a25eb5c64675a2a
722C <722C@users.noreply.github.com>
2018-11-07 19:46:57 -0700

    Add polyfill for old browsers (primarily IE).

--- payments_braintree_dropin/static/payments-braintree-dropin/dropin.js
+++ payments_braintree_dropin/static/payments-braintree-dropin/dropin.js
@@ -1,3 +1,18 @@
+// Element.closest() polyfill from MDN
+if (!Element.prototype.matches) Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+
+if (!Element.prototype.closest) {
+  Element.prototype.closest = function(s) {
+    var el = this;
+    if (!document.documentElement.contains(el)) return null;
+    do {
+      if (el.matches(s)) return el;
+      el = el.parentElement || el.parentNode;
+    } while (el !== null && el.nodeType === 1);
+    return null;
+  };
+}
+
 window.addEventListener('load', function() {
   var inputField = document.getElementById('dropin-container');
   var container = document.createElement('div');
